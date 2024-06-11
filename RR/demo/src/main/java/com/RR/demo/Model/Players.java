2b00cabd7fa8f9901d4771e43b9a6c51c92be69e
Natali K <113907659+misaki555@users.noreply.github.com>
2024-06-11 15:12:52 +0200

    Models for Games and Players

--- RR/demo/src/main/java/com/RR/demo/Model/Players.java
+++ RR/demo/src/main/java/com/RR/demo/Model/Players.java
@@ -1,4 +1,28 @@
 package com.RR.demo.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "players")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Players {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "GAME_ID", referencedColumnName = "ID")
+    private Games game_id;
+
+    @Column(name = "PHASE_STATUS")
+    private boolean phase_status;
 }
